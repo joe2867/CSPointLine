f0eb23d2040d60db4a4f4c356ebf3a65a4a953b1
joe2867 <jskelly@stu.socsd.org>
2025-01-15 23:37:16 -0500

    Update TestPoint.java

--- TestPoint.java
+++ TestPoint.java
@@ -6,6 +6,12 @@ public class TestPoint {
         Point p4 = new Point(3, -10);
         Point p5 = new Point(3, 10);
         Point p6 = new Point(6, 8);
+        System.out.println("P1: " + p1.getX() + "," + p1.getY());
+        System.out.println("P2: " + p2.getX() + "," + p2.getY());
+        System.out.println("P3: " + p3.getX() + "," + p3.getY());
+        System.out.println("P4: " + p4.getX() + "," + p4.getY());
+        System.out.println("P5: " + p5.getX() + "," + p5.getY());
+        System.out.println("P6: " + p6.getX() + "," + p6.getY());
         System.out.println("p2 to p3: " + p2.ManhattenDistance(p3));
         System.out.println("p3 to p4: " + p3.ManhattenDistance(p4));
         System.out.println("p2, p4, p5: " + p2.isCollinear(p4, p5));
